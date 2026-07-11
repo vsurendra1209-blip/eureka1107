eb820771ff6cb40277e72ae3bbe9cd8125c19715
Vsurendra <vsurendra9020@gmail.com>
2026-07-11 21:39:57 +0530

    all

---
+++ src/test/java/com/lerner/eureka/LernerEurekaApplicationTests.java
@@ -0,0 +1,14 @@
+package com.lerner.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LernerEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// This test just verifies that the application context loads without errors
+	}
+
+}
